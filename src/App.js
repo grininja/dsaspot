6c3bca85bd0eae657aed0eeb69ac006436f29160
grininja <raishubham675@gmail.com>
2021-05-10 11:45:05 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import {
   getData,
   updateDBData,
