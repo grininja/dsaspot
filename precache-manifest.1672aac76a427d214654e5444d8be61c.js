83b4e84bd07759326e00fc55be31e9df1972e654
grininja <raishubham675@gmail.com>
2021-05-10 11:42:40 +0530

    Updates

---
+++ precache-manifest.1672aac76a427d214654e5444d8be61c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff58cbec5d3f6e127e178f25a624d42",
+    "url": "/dsaspot/index.html"
+  },
+  {
+    "revision": "6cfc33a2e4b89fd88987",
+    "url": "/dsaspot/static/css/2.a07d8c99.chunk.css"
+  },
+  {
+    "revision": "d33fd9746ffb3f06babc",
+    "url": "/dsaspot/static/css/main.ec31c76d.chunk.css"
+  },
+  {
+    "revision": "6cfc33a2e4b89fd88987",
+    "url": "/dsaspot/static/js/2.b6807021.chunk.js"
+  },
+  {
+    "revision": "23c6e8c636923010bc877d2bf36960d8",
+    "url": "/dsaspot/static/js/2.b6807021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d33fd9746ffb3f06babc",
+    "url": "/dsaspot/static/js/main.6ffcea09.chunk.js"
+  },
+  {
+    "revision": "4f6e79a7048947f8e3d2",
+    "url": "/dsaspot/static/js/runtime-main.efba37d0.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/dsaspot/precache-manifest.1672aac76a427d214654e5444d8be61c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dsaspot/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
